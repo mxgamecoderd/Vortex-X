c36152890950898ffb7e54619c5d36058440f243
mxgamecoder <OlamilekanDamilaraa@gmail.com>
2024-12-16 19:04:23 -0500

    Update vv.js

--- Cmds/General/vv.js
+++ Cmds/General/vv.js
@@ -12,11 +12,11 @@ if (m.quoted.message) {
             if (/video/.test(type)) {
 
 
-               await client.sendMessage(m.chat, { video: media, caption: Retrieved by Vortex-X\nOriginal caption: ${q.caption}}, { quoted: m})
+               await client.sendMessage(m.chat, { video: media, caption:` Retrieved by Vortex-X\nOriginal caption: ${q.caption}`}, { quoted: m})
 
             } else if (/image/.test(type)) {
 
-await client.sendMessage(m.chat, { image: media, caption: Retrieved by MX-GΔMΞCØDΞR\nOriginal caption: ${q.caption}}, { quoted: m})
+await client.sendMessage(m.chat, { image: media, caption: `Retrieved by MX-GΔMΞCØDΞR\nOriginal caption: ${q.caption}`}, { quoted: m})
 
             }
          } else m.reply("That is not a viewonce media. . .")
