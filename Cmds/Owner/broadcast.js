ad033549c2863a59dab29932cee9f5fe87ab16ea
mxgamecoder <OlamilekanDamilaraa@gmail.com>
2024-12-17 13:07:24 +0100

    Update broadcast.js

--- Cmds/Owner/broadcast.js
+++ Cmds/Owner/broadcast.js
@@ -18,7 +18,7 @@ await m.reply("sending broadcast message...")
 for (let i of res) { 
 
 
-let txt = `BROADCAST MESSAGE (dreaded) \n\n🀄 Message: ${text}\n\nWritten by: ${pushname}` 
+let txt = `BROADCAST MESSAGE (Vortex-X) \n\n🀄 Message: ${text}\n\nWritten by: ${pushname}` 
 
 await client.sendMessage(i, { 
                  image: { 
